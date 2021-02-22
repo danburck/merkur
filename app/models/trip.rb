284081a171495c63945ff277b9166747a22cac3e
danburck <daniel.m.burckhardt@gmail.com>
2021-02-22 19:04:33 +0100

    add trip validations

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,11 @@
 class Trip < ApplicationRecord
   belongs_to :body
   belongs_to :traveller
+
+  validates :body, presence: true
+  validates :traveller, presence: true
+  validates :arrival_date, presence: true
+  validates :departure_date, presence: true
+  validates :starship, presence: true
+  validates :travel_cost, presence: true
 end
