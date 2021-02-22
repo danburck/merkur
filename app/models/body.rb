3e9e392c0f40b2c42490d02ef0983ff9dd4a5397
danburck <daniel.m.burckhardt@gmail.com>
2021-02-22 18:55:56 +0100

    add associations

--- app/models/body.rb
+++ app/models/body.rb
@@ -1,4 +1,6 @@
 class Body < ApplicationRecord
-	validates :name, :nearest_planet, :distance, :climate, :discovery_date, :discovered_by, :is_planet, :gravity, :capacity, :cost_per_day, presence: true
+  has_many :trips
+
+  validates :name, :nearest_planet, :distance, :climate, :discovery_date, :discovered_by, :is_planet, :gravity, :capacity, :cost_per_day, presence: true
 	validates :capacity, :cost_per_day, :numericality => { :greater_than_or_equal_to => 1 }
 end
--- app/models/traveller.rb
+++ app/models/traveller.rb
@@ -1,6 +1,6 @@
 class Traveller < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :trips
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :first_name, presence: true
