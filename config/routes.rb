2413f4086d99644920336ae6256b7ffc4cd72923
danburck <daniel.m.burckhardt@gmail.com>
2021-02-23 12:06:44 +0100

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :travellers
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :planets, only: [:show]
 end
