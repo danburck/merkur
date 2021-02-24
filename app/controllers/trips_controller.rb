97dd053f24d0fe5e801c07c2c50f657592f28162
Davide Tello <tello.davide@gmail.com>
2021-02-24 11:26:54 +0100

    Add Trips Controller und Update Trip Model

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,17 @@
+class TripsController < ApplicationController
+  def create
+    @trip = Trip.new(trip_params)
+    @trip.body_id = params[:body_id]
+    @trip.user = current_user
+
+    if @dtrip.save
+      redirect_to trip_path(params[:trip_id])
+    else
+      render :new
+    end
+  end
+
+  def trip_params
+    params.require(:trip).permit(:arrival_date, :departure_date, :user_id, :body_id, :travel_cost, :starship)
+  end
+end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,10 @@
 class Trip < ApplicationRecord
   belongs_to :body
   belongs_to :user
+
+  validates :arrival_date, presence: true
+  validates :departure_date, presence: true
+  validates :starship, presence: true
+  validates :travel_cost, presence: true
+  validates :starship, presence: true
 end
