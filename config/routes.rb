6c5263cc3855a52155bfa92af03902e18f3a5d81
Davide Tello <tello.davide@gmail.com>
2021-02-24 16:04:38 +0100

    Add basic confirmation page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-resources :trips, only: :index
+resources :trips, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :bodies, only: [ :index, :show, :new, :create ] do
     resources :trips, only: [ :index,:new, :create ]
