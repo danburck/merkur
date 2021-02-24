a4753900239f5c46061a542f31bff847d972a586
Adrienne W <ovska@protonmail.com>
2021-02-24 13:59:21 +0100

    change reference of starship

--- app/models/starship.rb
+++ app/models/starship.rb
@@ -1,5 +1,5 @@
 class Starship < ApplicationRecord
-	has_many :starships
+	has_many :bodies
 	validates :name, :cost, presence: true
 	validates :name, uniqueness: true
 	validates :cost, :numericality => { :greater_than_or_equal_to => 1 }
