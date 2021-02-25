0b89fdb3ee6e936a314bc7945fb4aed2117752be
Davide Tello <tello.davide@gmail.com>
2021-02-25 13:58:58 +0100

    Add link to the respective show page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
     @bodies = Body.all
+    @moon =  Body.where(name: 'moon')
+    @mars =  Body.where(name: 'mars')
   end
 end
