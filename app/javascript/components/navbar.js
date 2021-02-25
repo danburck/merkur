cb6ad5491dc7a4da45f22f94e9afec88e94a9815
Davide Tello <tello.davide@gmail.com>
2021-02-25 16:11:18 +0100

    Fix color for the hamburger

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,15 +2,18 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lewagon');
   const text = document.querySelectorAll('.nav-link');
   const logo = document.querySelector('.logo-white')
+  const hamburger = document.querySelector(".navbar")
   if (navbar) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= 400) {
         navbar.classList.add('navbar-lewagon-white');
         logo.classList.remove('logo-white');
+        hamburger.classList.add("navbar-light");
         text.forEach(element => element.classList.add('nav-link-white'));
       } else {
         navbar.classList.remove('navbar-lewagon-white');
         logo.classList.add('logo-white');
+        hamburger.classList.remove("navbar-dark");
         text.forEach(element => element.classList.remove('nav-link-white'));
       }
     });
