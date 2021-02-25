f8d6f378d3af41102640ce1b322222ffb2571d90
Davide Tello <tello.davide@gmail.com>
2021-02-25 15:54:24 +0100

    Add new logo with transparent background and update the JS function

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,13 +1,16 @@
 const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lewagon');
   const text = document.querySelectorAll('.nav-link');
+  const logo = document.querySelector('.logo-white')
   if (navbar) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= 400) {
         navbar.classList.add('navbar-lewagon-white');
+        logo.classList.remove('logo-white');
         text.forEach(element => element.classList.add('nav-link-white'));
       } else {
         navbar.classList.remove('navbar-lewagon-white');
+        logo.classList.add('logo-white');
         text.forEach(element => element.classList.remove('nav-link-white'));
       }
     });
